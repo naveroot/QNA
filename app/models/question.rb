256393fe264fd669f689a8481014d6105b432eb5
naveroot <naveroot@gmail.com>
2018-10-19 17:18:06 +0300

    Add dependents to model, migrations, add validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ApplicationRecord
+  has_many :answers, dependent: :destroy
+
+  validates :body, :title, presence: true
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  it { should validates_presence_of :body }
+  it { should validate_presence_of :body }
 
   it { should belong_to :question }
 
